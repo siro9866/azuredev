c4f24ea03b71ee53942bd698eab6304ff4269277
leekw <leekw@seowoninfo.com>
2025-02-24 17:35:58 +0900

    배포변경

--- src/main/java/com/seowoninfo/azuredev/MainController.java
+++ src/main/java/com/seowoninfo/azuredev/MainController.java
@@ -1,9 +1,13 @@
 package com.seowoninfo.azuredev;
 
+import com.seowoninfo.azuredev.util.AES256;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * FileName    : IntelliJ IDEA
@@ -16,15 +20,34 @@ public class MainController {
 
     @Value("${spring.profiles.active}") private String PROP;
 
-
     @GetMapping("/main")
-    public String main(Model model) {
+    public String main(Model model) throws Exception {
 
-        model.addAttribute("prop", PROP);
+
+        String plainTxt = "이거슨 한글입니다.";
+        String encTxt = "";
+
+        AES256 aes256 = new AES256("abcdefghijklmnop");
+
+
+        encTxt = jasyptEncoding(plainTxt);
 
         System.out.println("PROP================================= " + PROP);
+        System.out.println("plainTxt================================= " + plainTxt);
+        System.out.println("encTxt================================= " + encTxt);
+
+        model.addAttribute("prop", PROP);
+        model.addAttribute("plainTxt", plainTxt);
+        model.addAttribute("encTxt", encTxt);
 
         return "main";
+    }
+
+    public String jasyptEncoding(String value) {
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+        pbeEnc.setPassword("ThisIsSilFrameworkAndThisFrameworkIsBorn20250101BySil");
+        return pbeEnc.encrypt(value);
     }
 
 }
